68362c962b481f3b10d9a32837eed09dcb71bfcb
brminnick <brandon.r.minnick@gmail.com>
2017-11-13 09:04:50 -0800

    Updated FinishedLaunching Parameters

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -10,14 +10,14 @@ namespace FacialRecognitionLogin.iOS
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             global::Xamarin.Forms.Forms.Init();
             EntryCustomReturn.Forms.Plugin.iOS.CustomReturnEntryRenderer.Init();
 
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            return base.FinishedLaunching(uiApplication, launchOptions);
         }
     }
 }
