19f38f3b9f8ade1117b4e19cf16d9a67b5fd6181
brminnick <brandon.r.minnick@gmail.com>
2017-12-29 10:19:55 -0500

    Updated CustomRenderer for Xamarin.Forms 2.5.0

--- Droid/CustomRenderers/FontAwesomeIconRenderer.cs
+++ Droid/CustomRenderers/FontAwesomeIconRenderer.cs
@@ -1,21 +1,36 @@
-﻿using Android.Graphics;
+﻿using Android.Content;
+using Android.Graphics;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
 using FacialRecognitionLogin;
 using FacialRecognitionLogin.Droid;
+using Plugin.CurrentActivity;
 
 [assembly: ExportRenderer(typeof(FontAwesomeIcon), typeof(FontAwesomeIconRenderer))]
 namespace FacialRecognitionLogin.Droid
 {
     public class FontAwesomeIconRenderer : LabelRenderer
     {
+        #region Constructors
+        public FontAwesomeIconRenderer(Context context) : base(context)
+        {
+        }
+        #endregion
+
+        #region Properties
+        Context CurrentContext => CrossCurrentActivity.Current.Activity;
+        #endregion
+
+        #region Methods
         protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
         {
             base.OnElementChanged(e);
+
             if (e.OldElement == null)
-                Control.Typeface = Typeface.CreateFromAsset(Forms.Context.Assets, "FontAwesome.ttf");
+                Control.Typeface = Typeface.CreateFromAsset(CurrentContext.Assets, "FontAwesome.ttf");
         }
+        #endregion
     }
 }
