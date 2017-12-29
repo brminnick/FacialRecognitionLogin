da4b1cc00a205d67826eae1380276fe7cd47a0b8
brminnick <brandon.r.minnick@gmail.com>
2017-12-29 11:47:41 -0500

    Updated Permission Plugin

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,13 +1,21 @@
-﻿
-using Android.OS;
+﻿using Android.OS;
 using Android.App;
+using Android.Runtime;
 using Android.Content.PM;
+
+using Plugin.Permissions;
 
 namespace FacialRecognitionLogin.Droid
 {
     [Activity(Label = "FacialRecognitionLogin.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
