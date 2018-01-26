6d8fc07b769b9d4acdd679756c191f6bf7d3bd43
brminnick <brandon.r.minnick@gmail.com>
2018-01-26 10:50:40 -0800

    Updated EntryCustomReturn NuGet

--- FacialRecognitionLogin/Constants/AzureConstants.cs
+++ FacialRecognitionLogin/Constants/AzureConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class AzureConstants
     {
-        #warning Create your own API Key Here: https://aka.ms/Axnics
+#warning Create your own API Key Here: https://aka.ms/Axnics
         public const string FacialRecognitionAPIKey = "1034a4230bee4c6d962032cf5ce82766";
     }
 }
